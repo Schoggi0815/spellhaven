96f655e6cfeba21e049178e1a4fd7a48552aa330
Schoggi <matt.bossi@bluewin.ch>
2025-11-21 22:08:02 +0100

    Fix the buffer to actually work lol

--- crates/physics/src/network_physics_buffer.rs
+++ crates/physics/src/network_physics_buffer.rs
@@ -1,29 +1,137 @@
 use bevy::prelude::*;
 use bevy_hookup_core::from_session::FromSession;
+use itertools::{FoldWhile, Itertools};
 
 use crate::network_physics_object::NetworkPhysicsObject;
 
-const BUFFER_SIZE: usize = 4;
+const BUFFER_SIZE: usize = 3;
 
 #[derive(Component, Default, Debug, Reflect)]
 #[require(Transform)]
 pub struct NetworkPhysicsBuffer {
-    pub buffer: [Vec3; BUFFER_SIZE + 1],
+    pub buffer: [Option<Vec3>; BUFFER_SIZE],
     pub current_network_index: u64,
-    pub latest_velocity: Vec3,
+    pub actual_current: Vec3,
+    pub actual_last: Vec3,
 }
 
 impl NetworkPhysicsBuffer {
-    fn next(&mut self, next_pos: Vec3) {
-        for i in 0..BUFFER_SIZE {
+    fn try_pop(&mut self) {
+        let Some(first) = self.buffer[0] else {
+            info!("Skipping one buffer cycle");
+            return;
+        };
+
+        self.actual_last = self.actual_current;
+        self.actual_current = first;
+
+        for i in 0..BUFFER_SIZE - 1 {
             self.buffer[i] = self.buffer[i + 1];
         }
 
-        self.buffer[BUFFER_SIZE] = next_pos;
+        self.buffer[BUFFER_SIZE - 1] = None;
+        self.current_network_index += 1;
+    }
+
+    fn set_position(&mut self, network_index: u64, position: Vec3) {
+        let array_index = (network_index - self.current_network_index) as usize;
+        info!(
+            "network: {}, current_network: {}",
+            network_index, self.current_network_index
+        );
+
+        if array_index < BUFFER_SIZE {
+            self.buffer[array_index] = Some(position);
+
+            if array_index > 0 && self.buffer[array_index - 1].is_none() {
+                let (latest_position, latest_index) = self.get_latest();
+
+                for i in (latest_index + 1) as usize..array_index {
+                    let lerp_value = (i as f32 - latest_index as f32)
+                        / (array_index as f32 - latest_index as f32);
+
+                    self.buffer[i] =
+                        Some(latest_position.lerp(position, lerp_value));
+                }
+            }
+
+            return;
+        }
+
+        let shift = array_index - BUFFER_SIZE + 1;
+        let array_index = BUFFER_SIZE - 1;
+        info!("Shifting up buffer by: {}", shift);
+
+        let (latest_position, latest_index) = self.get_latest();
+        let latest_index = latest_index - shift as i32;
+
+        let lerp_max = array_index as f32 - latest_index as f32;
+
+        if shift <= 1 {
+            self.actual_last = self.actual_current;
+        } else {
+            let actual_last_index = shift - 2;
+
+            let actual_last =
+                self.buffer.get(actual_last_index).cloned().flatten();
+
+            self.actual_last = if let Some(actual_last) = actual_last {
+                actual_last
+            } else {
+                latest_position.lerp(
+                    position,
+                    (actual_last_index as f32 - latest_index as f32) / lerp_max,
+                )
+            };
+        }
+
+        let actual_current_index = shift - 1;
+        let actual_current =
+            self.buffer.get(actual_current_index).cloned().flatten();
+
+        self.actual_current = if let Some(actual_current) = actual_current {
+            actual_current
+        } else {
+            latest_position.lerp(
+                position,
+                (actual_current_index as f32 - latest_index as f32) / lerp_max,
+            )
+        };
+
+        for i in 0..BUFFER_SIZE - 1 {
+            let before_index = i + shift;
+            let new = self.buffer.get(before_index).cloned().flatten();
+
+            self.buffer[i] = if let Some(new) = new {
+                Some(new)
+            } else {
+                Some(latest_position.lerp(
+                    position,
+                    (before_index as f32 - latest_index as f32) / lerp_max,
+                ))
+            };
+        }
+
+        self.buffer[array_index] = Some(position);
+        self.current_network_index += shift as u64;
+    }
+
+    fn get_latest(&self) -> (Vec3, i32) {
+        self.buffer
+            .iter()
+            .enumerate()
+            .fold_while((self.actual_current, -1), |acc, (index, current)| {
+                if let Some(current) = current {
+                    FoldWhile::Continue((*current, index as i32))
+                } else {
+                    FoldWhile::Done(acc)
+                }
+            })
+            .into_inner()
     }
 
     fn actual_position(&self, lerp: f32) -> Vec3 {
-        self.buffer[0].lerp(self.buffer[1], lerp)
+        self.actual_last.lerp(self.actual_current, lerp)
     }
 }
 
@@ -35,46 +143,30 @@ pub fn add_buffer(
     mut commands: Commands,
 ) {
     for (entity, network_object) in shared_physics {
+        let mut buffer = [None; BUFFER_SIZE];
+        buffer[0] = Some(network_object.position);
+
         commands.entity(entity).insert(NetworkPhysicsBuffer {
-            buffer: [network_object.position; BUFFER_SIZE + 1],
+            buffer,
             current_network_index: network_object.update_index,
-            latest_velocity: network_object.velocity,
+            actual_current: network_object.position,
+            actual_last: network_object.position,
         });
     }
 }
 
 pub fn update_network_physics_buffer(
     smoothings: Query<(&mut NetworkPhysicsBuffer, Ref<NetworkPhysicsObject>)>,
-    time: Res<Time>,
 ) {
     for (mut network_buffer, network_object) in smoothings {
-        if network_object.is_changed()
-            && network_object.update_index
-                > network_buffer.current_network_index
-        {
-            network_buffer.current_network_index = network_object.update_index;
-            network_buffer.next(network_object.position);
-            network_buffer.latest_velocity = network_object.velocity;
-            continue;
+        if network_object.is_changed() {
+            network_buffer.set_position(
+                network_object.update_index,
+                network_object.position,
+            );
         }
 
-        let next_pos = network_buffer.buffer[BUFFER_SIZE]
-            + network_buffer.latest_velocity * time.delta_secs();
-        network_buffer.next(next_pos);
-
-        if network_object.is_changed()
-            && network_buffer.current_network_index >= BUFFER_SIZE as u64
-            && network_object.update_index
-                >= network_buffer.current_network_index - BUFFER_SIZE as u64
-            && network_object.update_index
-                < network_buffer.current_network_index
-        {
-            let buffer_index = BUFFER_SIZE as u64 + network_object.update_index
-                - network_buffer.current_network_index;
-
-            network_buffer.buffer[buffer_index as usize] =
-                network_object.position;
-        }
+        network_buffer.try_pop();
     }
 }
 
